5f911d0b9942a8b875a47be35b57b632dfdcedcc
Asirin85 <68087652+Asirin85@users.noreply.github.com>
2020-12-13 17:32:37 +0400

    Add files via upload

--- src/app/train/train.component.ts
+++ src/app/train/train.component.ts
@@ -1,41 +1,96 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 import Keyboard from "simple-keyboard";
 import Layout from "simple-keyboard-layouts/build/layouts/russian"
+
 @Component({
   selector: 'app-root',
   encapsulation: ViewEncapsulation.None,
   templateUrl: './train.component.html',
-  styleUrls: ['./train.component.scss',"../../../node_modules/simple-keyboard/build/css/index.css"]
+  styleUrls: ['./train.component.scss', "../../../node_modules/simple-keyboard/build/css/index.css"]
 })
 export class TrainComponent {
   value = "";
   keyboard: Keyboard;
+  errorCount = 0;
+  exercise: string = "тест тест тест тест";
+  symbolCount: number = 0;
+
+
   ngAfterViewInit() {
     this.keyboard = new Keyboard({
-      onChange: input => this.onChange(input),
-      onKeyPress: button => this.onKeyPress(button),
-      physicalKeyboardHighlight:true,
-      layout: Layout
+      disableButtonHold: true,
+      physicalKeyboardHighlight: true,
+      physicalKeyboardHighlightPress: true,
+      layout: Layout,
+      buttonTheme: [
+        {
+          class: "zone1",
+          buttons: "5 6 7 к е н г а п р о м и т ь"
+        },
+        {
+          class: "zone2",
+          buttons: "4 8 у ш в л с б"
+        },
+        {
+          class: "zone3",
+          buttons: "3 9 ц щ ы д ч ю"
+        },
+        {
+          class: "zone4",
+          buttons: "1 2 0 - = й з х ъ ф ж э я /"
+        },
+        {
+          class: "space",
+          buttons: "{space}"
+        }
+      ]
     });
   }
 
-  onChange = (input: string) => {
-    this.value = input;
-    console.log("Input changed", input);
+  changeColor() {
+    if (this.exercise[0] == " ") {
+      this.keyboard.removeButtonTheme("{space}", "nextChar");
+    } else this.keyboard.removeButtonTheme(this.exercise[0], "nextChar");
+    this.exercise = this.exercise.substring(1);
+    if (this.exercise[0] == " ") {
+      this.keyboard.addButtonTheme("{space}", "nextChar");
+    } else this.keyboard.addButtonTheme(this.exercise[0], "nextChar");
+  }
+  speed:string = "0";
+  seconds: number = 0;
+  minutes: number = 0;
+  interval;
+
+  startTimer() {
+    this.interval = setInterval(() => {
+      if (this.seconds < 59) {
+        this.seconds++;
+        this.speed = (this.symbolCount / (this.minutes * 60 + this.seconds)).toFixed(2);
+      } else {
+        this.minutes++;
+        this.seconds = 0;
+      }
+    }, 1000);
+  }
+
+  pauseTimer() {
+    clearInterval(this.interval);
   }
 
   onKeyPress = (button: string) => {
-    console.log("Button pressed", button);
-
+    if (this.exercise.length > 0 && this.exercise[0] != button.replace("{space}", " ")) {
+      this.errorCount++;
+    }
+    this.changeColor();
+    this.symbolCount++;
+    if (this.exercise.length === 0) this.pauseTimer();
     /**
      * If you want to handle the shift and caps lock buttons
      */
     if (button === "{shift}" || button === "{lock}") this.handleShift();
+
   }
 
-  onInputChange = (event: any) => {
-    this.keyboard.setInput(event.target.value);
-  }
 
   handleShift = () => {
     let currentLayout = this.keyboard.options.layoutName;
@@ -45,5 +100,14 @@ export class TrainComponent {
       layoutName: shiftToggle
     });
   }
+
+  start() {
+    this.pauseTimer();
+    console.log("1");
+    this.startTimer();
+    this.keyboard.addButtonTheme(this.exercise[0], "nextChar");
+    this.keyboard.setOptions({onKeyPress: button => this.onKeyPress(button)});
+  }
+
   hidden = false;
 }
