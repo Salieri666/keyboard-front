2e9170c0f6db4a17ce6923f1ef15bcf771f23c5b
Asiri85 <asirin85@gmail.com>
2020-12-13 22:44:05 +0400

    Popup add

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {AuthserviceService} from '../auth/authservice.service';
 import {Router} from '@angular/router';
 import {RegistrationDto} from '../auth/RegistrationDto';
+import {ModalDismissReasons, NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-root',
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit {
 
 
   constructor(private auth: AuthserviceService,
-              private router: Router) {
+              private router: Router, private modalService: NgbModal) {
   }
 
   ngOnInit() {
@@ -44,22 +45,56 @@ export class LoginComponent implements OnInit {
   }
 
   public registration() {
-    if (this.passwordCheck.length >= 4 && this.passwordCheck.length <= 12 &&
-      this.credentials.username.length >= 4 && this.credentials.username.length <= 8 && this.credentials.password === this.passwordCheck) {
-      const specialcharRegex = new RegExp('[!@#$%^&*(),.?\":{}|<>]');
-      if (!specialcharRegex.test(this.passwordCheck) && !specialcharRegex.test(this.credentials.username)) {
-        this.auth.registration(new RegistrationDto(this.credentials.username, this.credentials.password, 'USER')).subscribe(() => {
-            console.log('success');
-          },
-          error => {
-            console.log(error);
+    this.popupMSG = "Регистрация выполнена успешно";
+    this.popupTitle="Операция выполнена";
+    if (this.credentials.username.length >= 4 && this.credentials.username.length <= 8) {
+      if (this.passwordCheck.length >= 4 && this.passwordCheck.length <= 12) {
+        if (this.credentials.password === this.passwordCheck) {
+          const specialcharRegex = new RegExp('[!@#$%^&*(),.?\":{}|<>]');
+          if (!specialcharRegex.test(this.passwordCheck) && !specialcharRegex.test(this.credentials.username)) {
+            this.auth.registration(new RegistrationDto(this.credentials.username, this.credentials.password, 'USER')).subscribe(() => {
+              },
+              () => {
+                this.popupMSG = "Пользователь с таким именем уже существует";
+                this.popupTitle="Ошибка";
+              }
+            );
+            this.credentials.password = '';
+            this.passwordCheck = '';
+            this.register = false;
           }
-        );
-        this.credentials.password = '';
-        this.passwordCheck = '';
-        this.register = false;
+        } else {
+          this.popupMSG = "Неправильно введена проверка пароля";
+          this.popupTitle="Ошибка";
+        }
+      } else {
+        this.popupMSG = "Длина пароля должна быть от 4 до 12 символов";
+        this.popupTitle="Ошибка";
       }
+    } else {
+      this.popupMSG = "Имя пользователя должно содержать от 4 до 8 символов";
+      this.popupTitle="Ошибка";
     }
+  }
 
+  closeResult: string;
+  popupMSG: string;
+  popupTitle: string;
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 }
