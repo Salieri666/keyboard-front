17c70e21cb4a5331e85952b9f6d4231ffca62b08
Asiri85 <asirin85@gmail.com>
2020-12-13 23:07:40 +0400

    Popup add

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -26,16 +26,35 @@ export class LoginComponent implements OnInit {
   }
 
   public login() {
-    this.auth.login(this.credentials.username, this.credentials.password).subscribe(value => {
-      localStorage.setItem('token', value.token);
-      localStorage.setItem('username', value.username);
-      localStorage.setItem('userRole', value.role);
-      localStorage.setItem('userLevelId', value.levelId);
-      localStorage.setItem('userId', value.id);
-      this.router.navigate(['help']);
-    }, error => {
-      console.log(error);
-    });
+    this.popupMSG = "Вход выполнен успешно";
+    this.popupTitle = "Операция выполнена";
+    if (this.credentials.username.length >= 4 && this.credentials.username.length <= 8) {
+      if (this.credentials.password.length >= 4 && this.credentials.password.length <= 12) {
+        const specialcharRegex = new RegExp('[!@#$%^&*(),.?\":{}|<>]');
+        if (!specialcharRegex.test(this.credentials.password) && !specialcharRegex.test(this.credentials.username)) {
+          this.auth.login(this.credentials.username, this.credentials.password).subscribe(value => {
+            localStorage.setItem('token', value.token);
+            localStorage.setItem('username', value.username);
+            localStorage.setItem('userRole', value.role);
+            localStorage.setItem('userLevelId', value.levelId);
+            localStorage.setItem('userId', value.id);
+            this.router.navigate(['help']);
+          }, () => {
+            this.popupMSG = "Ошибка входа";
+            this.popupTitle = "Ошибка";
+          });
+        } else {
+          this.popupMSG = "Логин и пароль не должны содержать специальных символов";
+          this.popupTitle = "Ошибка";
+        }
+      } else {
+        this.popupMSG = "Длина пароля должна быть от 4 до 12 символов";
+        this.popupTitle = "Ошибка";
+      }
+    } else {
+      this.popupMSG = "Имя пользователя должно содержать от 4 до 8 символов";
+      this.popupTitle = "Ошибка";
+    }
     this.authenticated = this.auth.islogin;
   }
 
@@ -46,40 +65,44 @@ export class LoginComponent implements OnInit {
 
   public registration() {
     this.popupMSG = "Регистрация выполнена успешно";
-    this.popupTitle="Операция выполнена";
+    this.popupTitle = "Операция выполнена";
     if (this.credentials.username.length >= 4 && this.credentials.username.length <= 8) {
-      if (this.passwordCheck.length >= 4 && this.passwordCheck.length <= 12) {
+      if (this.credentials.password.length >= 4 && this.credentials.password.length <= 12) {
         if (this.credentials.password === this.passwordCheck) {
           const specialcharRegex = new RegExp('[!@#$%^&*(),.?\":{}|<>]');
-          if (!specialcharRegex.test(this.passwordCheck) && !specialcharRegex.test(this.credentials.username)) {
+          if (!specialcharRegex.test(this.credentials.password) && !specialcharRegex.test(this.credentials.username)) {
             this.auth.registration(new RegistrationDto(this.credentials.username, this.credentials.password, 'USER')).subscribe(() => {
               },
               () => {
                 this.popupMSG = "Пользователь с таким именем уже существует";
-                this.popupTitle="Ошибка";
+                this.popupTitle = "Ошибка";
               }
             );
             this.credentials.password = '';
             this.passwordCheck = '';
             this.register = false;
+          } else {
+            this.popupMSG = "Логин и пароль не должны содержать специальных символов";
+            this.popupTitle = "Ошибка";
           }
         } else {
           this.popupMSG = "Неправильно введена проверка пароля";
-          this.popupTitle="Ошибка";
+          this.popupTitle = "Ошибка";
         }
       } else {
         this.popupMSG = "Длина пароля должна быть от 4 до 12 символов";
-        this.popupTitle="Ошибка";
+        this.popupTitle = "Ошибка";
       }
     } else {
       this.popupMSG = "Имя пользователя должно содержать от 4 до 8 символов";
-      this.popupTitle="Ошибка";
+      this.popupTitle = "Ошибка";
     }
   }
 
   closeResult: string;
   popupMSG: string;
   popupTitle: string;
+
   open(content) {
     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
@@ -88,7 +111,13 @@ export class LoginComponent implements OnInit {
     });
   }
 
-  private getDismissReason(reason: any): string {
+  private
+
+  getDismissReason(reason
+                     :
+                     any
+  ):
+    string {
     if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
