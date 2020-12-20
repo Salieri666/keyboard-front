135a64cb19e77324e45c9af5c32b92724d862485
Ilya Fisenko <Ilya.Fisenko@waveaccess.ru>
2020-12-20 18:16:33 +0400

    Added profile link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import {CommonModule} from "@angular/common";
+import {CommonModule} from '@angular/common';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {CustomHttpInterceptorService} from "./auth/custom-http-interceptor.service";
+import {CustomHttpInterceptorService} from './auth/custom-http-interceptor.service';
 
 @NgModule({
   declarations: [
