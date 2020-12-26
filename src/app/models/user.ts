abc8a59024b66f85eb090f3f6f9bc5fb2d1e1d84
Asiri85 <asirin85@gmail.com>
2020-12-26 17:28:27 +0400

    Testирование

--- src/app/models/user.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-  id:number;
-  username: string;
-  password:string;
-  active:number;
-  roles:string;
-  permissions:string;
-  levelId:number;
-}
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
-import {User} from "../models/user";
+import {Info} from "../auth/Info";
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +13,8 @@ export class UserService {
 
   changeLevel(level: number) {
     if (localStorage.getItem("userLevelId") == "null" || parseInt(localStorage.getItem("userLevelId")) < level) {
-      this.http.get(environment.apiUrl + '/user/getUser/' + parseInt(localStorage.getItem("userId"))).subscribe((data: User) => {
-        data.levelId = level;
+      this.http.get<Info>(environment.apiUrl + '/user/getUser/' + parseInt(localStorage.getItem("userId"))).subscribe((data: Info) => {
+        data.levelId = level.toString();
         this.http.post(environment.apiUrl + '/user/changeUserLevel', data).subscribe();
       });
       localStorage.removeItem('userLevelId');
